b73feecdfc136bfcd5f9055a141b2fb4e26c150f
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-06-11 09:34:36 +0200

    feat(CD): common folder

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,9 +14,11 @@ mkdir $TEMP_DIRECTORY || exit 1
 mkdir $TEMP_DIRECTORY/arjs || exit 1
 mkdir $TEMP_DIRECTORY/aframe-ar || exit 1
 mkdir $TEMP_DIRECTORY/3d-models || exit 1
+mkdir $TEMP_DIRECTORY/common || exit 1
 
 cp index.html $TEMP_DIRECTORY || exit 1
 
+cp -r common/* $TEMP_DIRECTORY/common || exit 1
 cp -r arjs/* $TEMP_DIRECTORY/arjs || exit 1
 cp -r 3d-models/* $TEMP_DIRECTORY/3d-models || exit 1
 cp -r aframe-ar/* $TEMP_DIRECTORY/aframe-ar || exit 1
