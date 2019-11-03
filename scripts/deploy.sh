16b7f78e8b961c89ebec422fa9a368c8975c2875
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2019-11-03 23:57:49 +0100

    Deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-echo "Starting deployment."
+echo "Prepare deployment."
 echo "Target: gh-pages branch"
 
-TARGET_BRANCH="master:gh-pages"
-
 TEMP_DIRECTORY="tmp"
-CURRENT_COMMIT=`git rev-parse HEAD`
-ORIGIN_URL=`git config --get remote.origin.url`
-ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
 
-echo "Compiling content."
+echo "Prepare content."
 mkdir $TEMP_DIRECTORY || exit 1
 mkdir $TEMP_DIRECTORY/arjs || exit 1
 mkdir $TEMP_DIRECTORY/aframe-ar || exit 1
@@ -26,18 +21,3 @@ cp -r aframe-ar/* $TEMP_DIRECTORY/aframe-ar || exit 1
 cp -r webxr/* $TEMP_DIRECTORY/webxr || exit 1
 
 cp .gitignore $TEMP_DIRECTORY || exit 1
-
-cd $TEMP_DIRECTORY
-
-git init && \
-
-echo "Pushing new content to $ORIGIN_URL"
-git config user.name "Travis-CI" || exit 1
-git config user.email "vincent.ogloblinsky@gmail.com" || exit 1
-
-git add -A . || exit 1
-git commit --allow-empty -m "Regenerated static content for $CURRENT_COMMIT" || exit 1
-git push --force "$ORIGIN_URL_WITH_CREDENTIALS" $TARGET_BRANCH > /dev/null 2>&1
-
-echo "Deployed successfully."
-exit 0
